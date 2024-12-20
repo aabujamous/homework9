f03b30ab520ecba2882d2d71648f24b39c5b9fd3
aabujamous <aabujamous2707@gmail.com>
2024-12-20 15:00:47 +0300

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program where the program takes a random integer between 1 and 10, and the user is then prompted to input a guess number. The program displays a message "Good Work"
+// if the input matches the guess number otherwise "Not matched".
+
+
+
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+
+const userGuess = parseInt(prompt("Guess a number between 1 and 10:"));
+
+
+if (userGuess === randomNumber) {
+    alert("Good Work");
+} else {
+    alert("Not matched.");
+}
